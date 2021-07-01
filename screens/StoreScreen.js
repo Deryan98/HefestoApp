a0ad042735491135cbce75bb43f6e6d70f494f2c
NeoCat98 <00076017@uca.edu.sv>
2021-07-01 12:14:04 -0600

    agregando datos para la pantalla de Tiendas

---
+++ screens/StoreScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Stores} from '../data/dummy-data';
+import ProductList from '../components/ProductList';
+
+
+const StoreScreen = ({route, navigation}) => {
+  //obtenemos el id de la subcategoria
+  const stores = Stores.find();
+  return (
+    <ProductList
+      route={route}
+      listData={stores}
+      navigation={navigation}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  StoreScreenContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default StoreScreen;
